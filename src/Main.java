e4a9ff5eed1b0f16c74f3361246963e7367d519f
tiep204 <dangtrantiep240604@gmail.com>
2023-06-07 19:36:56 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Thời Thời gian hiện tại là"+date);
+    }
+}
